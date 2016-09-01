1c62bf8d32cbdc2e28b6c6097bfb722d56dc1bbf
Jonah Bernhard <jonah.bernhard@gmail.com>
2016-09-01 16:59:21 -0400

    Improve EOS extrapolation.

--- src/eos.for
+++ src/eos.for
@@ -1,6 +1,5 @@
 #include "defs.h"
 
-! Ver 1.2: EOS extended to infinite energy density by extrapolation
       Subroutine InputRegulatedEOS
       Implicit none
 
@@ -9,20 +8,17 @@
       Integer, Parameter :: IMax_Mu = 40        !maximum allowed stable particles for partically chemical equilibrium EOS
 !=======list of parameters end===============================================
 
-!=======declear variables for reading equation of state======================
-      Integer :: I, J, K, L, M, N    !loop variables
+      Integer :: I
       double precision :: EOSe0=0.0d0   !lowest energy density
       double precision :: EOSde=0.0d0   !spacing of energy density
-      Integer :: EOSne=EOSDATALENGTH   !total rows of energy density
       double precision :: EOSEend   !the maximum energy density in the table
-      double precision :: ee
+      Integer :: EOSne=EOSDATALENGTH
       double precision :: PEOSdata(EOSDATALENGTH),
      &                    SEOSdata(EOSDATALENGTH),
      &                    TEOSdata(EOSDATALENGTH)
 
-      double precision :: logx0, logy0, logx1, logy1
-      double precision :: Pcoeff1, Pcoeff2, Scoeff1, Scoeff2,
-     &                    Tcoeff1, Tcoeff2
+      double precision :: e0, p0, cs2
+      double precision :: escale, epow, sscale, spow
 
       Integer :: Inumparticle = 0   !number of stable particles in PCE, 0 for chemical equilibrium EOS
       Integer :: EOS_Mu_ne = RegEOSMudatasize   !total rows in mu table
@@ -30,66 +26,65 @@
       double precision :: EOS_Mu_de = 0.0d0   !spacing of energy density in mu table
       double precision :: MuEOSdata(RegEOSMudatasize, IMax_Mu)
       Integer :: IMuflag
-!
-!=======declear variables for reading equation of state end==================
 
-!=======common blocks========================================================
       common /EOSdata/PEOSdata, SEOSdata, TEOSdata
       common /EOSdatastructure/ EOSe0, EOSde, EOSne, EOSEend
 
       common /EOSMudata/MuEOSdata, IMuflag
       common /EOSMudatastructure/ EOS_Mu_e0, EOS_Mu_de, EOS_Mu_ne,
      &                            Inumparticle
-      common /EOScoeffs/ Pcoeff1, Pcoeff2, Scoeff1, Scoeff2,
-     &                   Tcoeff1, Tcoeff2
-!=======common blocks end====================================================
+      common /EOScoeffs/ escale, epow, sscale, spow
+
 
       open(5,FILE='eos.dat',STATUS='OLD')
-      do I=1,EOSne
-        read(5,*) ee, PEOSdata(I), SEOSdata(I), TEOSdata(I)
-        if(I.eq.1) EOSe0 = ee
-        ! This is very dangerous!  Computing the step size like this,
-        ! from two adjacent entries, could cause significant error.
-        ! if(I.eq.2) EOSde = ee - EOSe0
-        ! It's much safer to divide the full range by the number of steps:
-        if(I.eq.EOSne) then
-          EOSde = (ee - EOSe0)/(EOSne-1)
-          EOSEend = ee
-        end if
+
+      ! read first row, save energy density as EOSe0
+      I = 1
+      read(5,*) EOSe0, PEOSdata(I), SEOSdata(I), TEOSdata(I)
+
+      ! read rest of table, save last energy density as EOSEend
+      do I=2,EOSne
+        read(5,*) EOSEend, PEOSdata(I), SEOSdata(I), TEOSdata(I)
       enddo
+
       close(5)
 
-      ! This is even crazier!  Just read it from the last table entry...
-      ! EOSEend = EOSe0 + EOSde*(EOSne-1)
+      ! save energy density step size
+      EOSde = (EOSEend - EOSe0)/(EOSne - 1)
 
-      ! Extrapolate the EOS table using a power law ansatz (a*x^b).
-      ! This works exceptionally well for the thermodynamic quantities
-      ! P, S, T at high energy density (well past the transition).
+      ! Prepare for extrapolating EOS beyond end of table.
+      ! Assume a modified power-law ansatz for pressure in the high
+      ! energy density limit:
       !
-      ! The coefficients (a, b) are easily derived by fitting the ansatz
-      ! to two points, i.e.
+      !   p(e) = e/3 + a*e^b
       !
-      !   y0 = a*x0^b,  y1 = a*x1^b
+      ! For b < 1, this converges to the ideal gas limit as e -> inf and
+      ! the speed of sound c_s^2 = dp/de -> 1/3 as e -> inf.
+      ! The coefficients (a, b) may be determined from the pressure and
+      ! its derivative (i.e. the speed of sound) at some reference
+      ! energy density e0.  Use the second-to-last table entry as the
+      ! reference point and estimate the derivative using a finite
+      ! difference.
+      e0 = EOSEend - EOSde
+      p0 = PEOSdata(EOSne - 1)
+      cs2 = (PEOSdata(EOSne) - PEOSdata(EOSne - 2)) / (2*EOSde)
+
+      ! Use variable 'epow' for the exponent 'b' and 'escale' for the
+      ! scale factor 'a'.
+      epow = e0*(1 - 3*cs2)/(e0 - 3*p0)
+      escale = (p0 - e0/3) * e0**(-epow)
+
+      ! Having extrapolated p(e), the entropy density follows from the
+      ! differential equation
       !
-      ! Compute the coefficients from the last table point and
-      ! another somewhat earlier point
-      logx0 = log(EOSEend - 10000*EOSde)
-      logx1 = log(EOSEend)
-
-      logy0 = log(PEOSdata(EOSne - 10000))
-      logy1 = log(PEOSdata(EOSne))
-      Pcoeff1 = exp((logx0*logy1 - logx1*logy0) / (logx0 - logx1))
-      Pcoeff2 = (logy0 - logy1) / (logx0 - logx1)
-
-      logy0 = log(SEOSdata(EOSne - 10000))
-      logy1 = log(SEOSdata(EOSne))
-      Scoeff1 = exp((logx0*logy1 - logx1*logy0) / (logx0 - logx1))
-      Scoeff2 = (logy0 - logy1) / (logx0 - logx1)
-
-      logy0 = log(TEOSdata(EOSne - 10000))
-      logy1 = log(TEOSdata(EOSne))
-      Tcoeff1 = exp((logx0*logy1 - logx1*logy0) / (logx0 - logx1))
-      Tcoeff2 = (logy0 - logy1) / (logx0 - logx1)
+      !   (e + p(e))/s(e) = 1/(ds(e)/de)
+      !
+      ! (Note: both sides of this equation are the temperature.)
+      ! For the chosen p(e) ansatz this diffeq can be analytically
+      ! integrated for s(e).  These coefficients (spow, sscale) are used
+      ! in SEOSL7, below.
+      spow = 3/(4*(1 - epow))
+      sscale = SEOSdata(EOSne - 1) * (e0**epow/(e0 + p0))**spow
 
       ! disable PCE
       !open(5,FILE='EOS/EOS_tables/EOS_particletable.dat', STATUS='OLD')
@@ -108,9 +103,9 @@
 
 
 C====EOS from table===================================================
-      Double Precision Function PEOSL7(ee)  ! for lattice P(e)
+      Double Precision Function PEOSL7(e)  ! for lattice P(e)
       Implicit none
-      double precision :: ee
+      double precision :: e
       double precision :: PEOSdata(EOSDATALENGTH),
      &                    SEOSdata(EOSDATALENGTH),
      &                    TEOSdata(EOSDATALENGTH)
@@ -118,30 +113,30 @@ C====EOS from table===================================================
       double precision :: EOSde         !spacing of energy density
       Integer :: EOSne                  !total rows of energy density
       double precision :: EOSEend   !the maximum energy density in the table
-      double precision :: Pcoeff1, Pcoeff2, Scoeff1, Scoeff2,
-     &                    Tcoeff1, Tcoeff2
+      double precision :: escale, epow, sscale, spow
 
       common /EOSdata/PEOSdata, SEOSdata, TEOSdata
       common /EOSdatastructure/ EOSe0, EOSde, EOSne, EOSEend
-      common /EOScoeffs/ Pcoeff1, Pcoeff2, Scoeff1, Scoeff2,
-     &                   Tcoeff1, Tcoeff2
+      common /EOScoeffs/ escale, epow, sscale, spow
 
-      ee = abs(ee)
+      e = abs(e)
 
-      if (ee.lt.EOSe0) then
-        PEOSL7 = PEOSdata(1)*ee/EOSe0
-      else if (ee.lt.EOSEend) then
-        call interpCubic(PEOSdata, EOSne, EOSe0, EOSde, ee, PEOSL7)
+      if (e.lt.EOSe0) then
+        PEOSL7 = PEOSdata(1)*e/EOSe0
+      else if (e.lt.EOSEend) then
+        call interpCubic(PEOSdata, EOSne, EOSe0, EOSde, e, PEOSL7)
       else
-        PEOSL7 = Pcoeff1*(ee**Pcoeff2)
+        ! extrapolate, see notes above
+        PEOSL7 = e/3 + escale*e**epow
       endif
 
       return
       end
 
-      Double Precision Function SEOSL7(ee)  ! for lattice S(e)
+      Double Precision Function SEOSL7(e)  ! for lattice S(e)
       Implicit none
-      double precision :: ee
+      double precision :: e
+      double precision :: PEOSL7
       double precision :: PEOSdata(EOSDATALENGTH),
      &                    SEOSdata(EOSDATALENGTH),
      &                    TEOSdata(EOSDATALENGTH)
@@ -149,30 +144,30 @@ C====EOS from table===================================================
       double precision :: EOSde         !spacing of energy density
       Integer :: EOSne                  !total rows of energy density
       double precision :: EOSEend   !the maximum energy density in the table
-      double precision :: Pcoeff1, Pcoeff2, Scoeff1, Scoeff2,
-     &                    Tcoeff1, Tcoeff2
+      double precision :: escale, epow, sscale, spow
 
       common /EOSdata/PEOSdata, SEOSdata, TEOSdata
       common /EOSdatastructure/ EOSe0, EOSde, EOSne, EOSEend
-      common /EOScoeffs/ Pcoeff1, Pcoeff2, Scoeff1, Scoeff2,
-     &                   Tcoeff1, Tcoeff2
+      common /EOScoeffs/ escale, epow, sscale, spow
 
-      ee = abs(ee)
+      e = abs(e)
 
-      if (ee.lt.EOSe0) then
-        SEOSL7 = SEOSdata(1)*ee/EOSe0
-      else if (ee.lt.EOSEend) then
-        call interpCubic(SEOSdata, EOSne, EOSe0, EOSde, ee, SEOSL7)
+      if (e.lt.EOSe0) then
+        SEOSL7 = SEOSdata(1)*e/EOSe0
+      else if (e.lt.EOSEend) then
+        call interpCubic(SEOSdata, EOSne, EOSe0, EOSde, e, SEOSL7)
       else
-        SEOSL7 = Scoeff1*(ee**Scoeff2)
+        ! extrapolate, see notes above
+        SEOSL7 = sscale * ((e + PEOSL7(e))/e**epow)**spow
       endif
 
       return
       end
 
-      Double Precision Function TEOSL7(ee)  ! for lattice T(e)
+      Double Precision Function TEOSL7(e)  ! for lattice T(e)
       Implicit none
-      double precision :: ee
+      double precision :: e
+      double precision :: PEOSL7, SEOSL7
       double precision :: PEOSdata(EOSDATALENGTH),
      &                    SEOSdata(EOSDATALENGTH),
      &                    TEOSdata(EOSDATALENGTH)
@@ -180,22 +175,22 @@ C====EOS from table===================================================
       double precision :: EOSde         !spacing of energy density
       Integer :: EOSne                  !total rows of energy density
       double precision :: EOSEend   !the maximum energy density in the table
-      double precision :: Pcoeff1, Pcoeff2, Scoeff1, Scoeff2,
-     &                    Tcoeff1, Tcoeff2
+      double precision :: escale, epow, sscale, spow
 
       common /EOSdata/PEOSdata, SEOSdata, TEOSdata
       common /EOSdatastructure/ EOSe0, EOSde, EOSne, EOSEend
-      common /EOScoeffs/ Pcoeff1, Pcoeff2, Scoeff1, Scoeff2,
-     &                   Tcoeff1, Tcoeff2
+      common /EOScoeffs/ escale, epow, sscale, spow
 
-      ee = abs(ee)
+      e = abs(e)
 
-      if (ee.lt.EOSe0) then
-        TEOSL7 = TEOSdata(1)*ee/EOSe0
-      else if (ee.lt.EOSEend) then
-        call interpCubic(TEOSdata, EOSne, EOSe0, EOSde, ee, TEOSL7)
+      if (e.lt.EOSe0) then
+        TEOSL7 = TEOSdata(1)*e/EOSe0
+      else if (e.lt.EOSEend) then
+        call interpCubic(TEOSdata, EOSne, EOSe0, EOSde, e, TEOSL7)
       else
-        TEOSL7 = Tcoeff1*(ee**Tcoeff2)
+        ! use extrapolated pressure and entropy density
+        ! T = (e + p)/s
+        TEOSL7 = (e + PEOSL7(e))/SEOSL7(e)
       endif
 
       return
